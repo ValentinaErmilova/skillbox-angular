e833e01fabdd66e7bb42075fb912912890527a1d
Valentina Fadina <vafa1019@netcracker.com>
2021-07-19 16:52:15 +0300

    ANG-3 homework 3.6

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,9 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
-import { products } from '../data/products.data';
-import { Product } from "../types/product";
-import { InCart } from "../types/inCart";
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { CartComponent } from "../cart/cart.component";
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {products} from '../data/products.data';
+import {Product} from "../types/product";
+import {InCart} from "../types/inCart";
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {CartComponent} from "../cart/cart.component";
 
 @Component({
   selector: 'app-catalog',
@@ -15,6 +15,12 @@ export class CatalogComponent implements OnInit {
   products: Product[] = products;
   public inCart: Array<InCart> = [];
   countInCart: number = 0;
+  selectedSortBy: string = '';
+  sortBy: List[] = [
+    {value: 'all', viewValue: 'Показать все'},
+    {value: 'exists', viewValue: 'В наличии'},
+    {value: 'discount', viewValue: 'Со скидкой'}
+  ];
 
   constructor(private modalService: NgbModal) {}
 
@@ -41,4 +47,31 @@ export class CatalogComponent implements OnInit {
     modalRef.componentInstance.inCart = this.inCart;
     modalRef.result.then((item) => this.removeItemToCart(item));
   }
+
+  sortBySelected(turnOn: any, sortBy: string) {
+    console.log(turnOn,  sortBy);
+    const data = this.products.slice();
+    if(!turnOn) {
+      this.products = data;
+      return;
+    }
+
+    this.products = data.sort((a, b) => {
+      switch (sortBy) {
+        case 'all': return compare(a.title, b.title);
+        case 'discount': return compare(a.price?.discount, b.price?.discount);
+        case 'exists': return compare(a.availability, b.availability);
+        default: return 0;
+      }
+    });
+  }
+}
+
+function compare(a: any, b: any) {
+  return (a < b ? -1 : 1) * 1;
+}
+
+interface List {
+  value: string;
+  viewValue: string;
 }
--- src/app/catalog/catalog.module.ts
+++ src/app/catalog/catalog.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CatalogComponent } from './catalog.component';
 import { ProductCardModule } from "../product-card/product-card.module";
+import {DropDownListModule} from "../drop-down-list/drop-down-list.module";
+import {ToggleModule} from "../toggle/toggle.module";
 
 @NgModule({
   declarations: [
@@ -9,7 +11,9 @@ import { ProductCardModule } from "../product-card/product-card.module";
   ],
   imports: [
     CommonModule,
-    ProductCardModule
+    ProductCardModule,
+    DropDownListModule,
+    ToggleModule
   ],
   exports: [
     CatalogComponent
